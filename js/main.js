4220afc5126bf3f5534238892ef766e27c2d6e4d
Israel Villa <israelvilla@comunidad.unam.mx>
2023-03-26 22:55:38 -0600

    add jquery

--- js/main.js
+++ js/main.js
@@ -1,36 +1,90 @@
-const aiButton = document.querySelector('#ai-button');
-const aiInfo = document.querySelector('#ai-info');
+class StickyNavigation {
+	
+	constructor() {
+		this.currentId = null;
+		this.currentTab = null;
+		this.tabContainerHeight = 70;
+		let self = this;
+		$('.et-hero-tab').click(function() { 
+			self.onTabClick(event, $(this)); 
+		});
+		$(window).scroll(() => { this.onScroll(); });
+		$(window).resize(() => { this.onResize(); });
+	}
+	
+	onScroll() {
+		this.checkTabContainerPosition();
+    this.findCurrentTabSelector();
+	}
+	
+	onResize() {
+		if(this.currentId) {
+			this.setSliderCss();
+		}
+	}
 
-aiButton.addEventListener('click', () => {
-  aiInfo.classList.toggle('hidden');
-  if (aiInfo.classList.contains('hidden')) {
-    aiButton.innerText = 'Ver más';
-    aiInfo.style.height = '0';
-  } else {
-    aiButton.innerText = 'Ver menos';
-    aiInfo.style.height = aiInfo.scrollHeight + 'px';
+  onTabClick(event, element) {
+    let scrollTop = $(element.attr('href')).offset().top - this.tabContainerHeight + 1;
   }
-});
+	
+  
+	checkTabContainerPosition() {
+		let offset = $('.et-hero-tabs').offset().top + $('.et-hero-tabs').height() - this.tabContainerHeight;
+		if($(window).scrollTop() > offset) {
+			$('.et-hero-tabs-container').addClass('et-hero-tabs-container--top');
+		} 
+		else {
+			$('.et-hero-tabs-container').removeClass('et-hero-tabs-container--top');
+		}
+	}
+	
+	findCurrentTabSelector(element) {
+		let newCurrentId;
+		let newCurrentTab;
+		let self = this;
+		$('.et-hero-tab').each(function() {
+			let id = $(this).attr('href');
+			let offsetTop = $(id).offset().top - self.tabContainerHeight;
+			let offsetBottom = $(id).offset().top + $(id).height() - self.tabContainerHeight;
+			if($(window).scrollTop() > offsetTop && $(window).scrollTop() < offsetBottom) {
+				newCurrentId = id;
+				newCurrentTab = $(this);
+			}
+		});
+		if(this.currentId != newCurrentId || this.currentId === null) {
+			this.currentId = newCurrentId;
+			this.currentTab = newCurrentTab;
+			this.setSliderCss();
+		}
+	}
+	
+	setSliderCss() {
+		let width = 0;
+		let left = 0;
+		if(this.currentTab) {
+			width = this.currentTab.css('width');
+			left = this.currentTab.offset().left;
+		}
+		$('.et-hero-tab-slider').css('width', width);
+		$('.et-hero-tab-slider').css('left', left);
+	}
+	
+}
 
+new StickyNavigation();
 
-const djangoButton = document.querySelector('#django-button');
-const djangoInfo = document.querySelector('#django-info');
-
-djangoButton.addEventListener('click', () => {
-  djangoInfo.classList.toggle('hidden');
-  if (djangoInfo.classList.contains('hidden')) {
-    djangoButton.innerText = 'Ver más';
-    djangoInfo.style.height = '0';
-  } else {
-    djangoButton.innerText = 'Ver menos';
-    djangoInfo.style.height = aiInfo.scrollHeight + 'px';
-  }
-});
-
-
-gsap.from('.section', {
-  opacity: 0,
-  y: 100,
-  stagger: 0.3,
-  duration: 1
+$(document).ready(function() {
+	var animated = false;
+	$(window).scroll(function() {
+		var scrollTop = $(this).scrollTop();
+		var sectionOffset = $('.section-progress').offset().top;
+		var sectionHeight = $('.section-progress').height();
+		if (scrollTop >= sectionOffset - $(window).height() && scrollTop <= sectionOffset + sectionHeight && !animated) {
+			$(".progress-animate .progress-bar").each(function() {
+			var width = $(this).attr("aria-valuenow") + "%";
+			$(this).animate({ width: width }, 1500);
+			});
+			animated = true;
+		}
+	});
 });
